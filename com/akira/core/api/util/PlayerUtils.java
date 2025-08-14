1645338f68bab2e2f599b7113469cdd126172048
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-08-14 16:42:09 +0800

    更新README.md；为PlayerUtils添加playEffect方法。

--- com/akira/core/api/util/PlayerUtils.java
+++ com/akira/core/api/util/PlayerUtils.java
@@ -1,6 +1,8 @@
 package com.akira.core.api.util;
 
 import org.apache.commons.lang3.Validate;
+import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -19,5 +21,27 @@ public class PlayerUtils {
 
     public static void playSound(Player player, Sound sound) {
         playSound(player, sound, 1.0F);
+    }
+
+    public static <T> void playEffect(Location location, Effect effect, T data) {
+        playEffect(location, effect, data, 64);
+    }
+
+    public static <T> void playEffect(Location location, Effect effect, T data, int radius) {
+        Validate.notNull(location);
+        Validate.notNull(effect);
+        Validate.isTrue(radius >= 0, "Radius cannot be lower than 0.");
+
+        World world = location.getWorld();
+        Validate.notNull(world, "World from the location instance is null.");
+        world.playEffect(location, effect, data, radius);
+    }
+
+    public static <T> void playEffect(Player player, Effect effect, T data, int radius) {
+        playEffect(player.getLocation(), effect, data, radius);
+    }
+
+    public static <T> void playEffect(Player player, Effect effect, T data) {
+        playEffect(player.getLocation(), effect, data);
     }
 }
