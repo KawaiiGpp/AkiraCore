fb96a198e63c792b082f18f53b12f1cbf6be9ebe
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2025-08-25 20:46:22 +0800

    为WorldUtils.playParticle新增重载并将extra默认值设置回0。

--- com/akira/core/api/util/WorldUtils.java
+++ com/akira/core/api/util/WorldUtils.java
@@ -23,6 +23,14 @@ public class WorldUtils {
         playSound(location, sound, 1.0F);
     }
 
+    public static void playParticle(Location location, Particle particle, int amount, double offset, double ex) {
+        playParticle(location, particle, amount, offset, offset, offset, ex, null);
+    }
+
+    public static void playParticle(Location location, Particle particle, int amount, double oX, double oY, double oZ, double ex) {
+        playParticle(location, particle, amount, oX, oY, oZ, ex, null);
+    }
+
     public static void playParticle(Location location, Particle particle, int amount, double oX, double oY, double oZ, double ex, Object data) {
         Validate.notNull(location);
         Validate.notNull(particle);
@@ -37,7 +45,7 @@ public class WorldUtils {
     }
 
     public static void playParticle(Location location, Particle particle, int amount, double oX, double oY, double oZ, Object data) {
-        playParticle(location, particle, amount, oX, oY, oZ, 1.0, data);
+        playParticle(location, particle, amount, oX, oY, oZ, 0, data);
     }
 
     public static void playParticle(Location location, Particle particle, int amount, double offset, Object data) {
